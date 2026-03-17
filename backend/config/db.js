94d4a6b86ba13830a53f2c01ed89ed62b92df475
moazalkhawaga88-del <moazalkhawaga88@gmail.com>
2026-03-17 09:11:08 +0200

    Create db.js for MongoDB connection configuration using mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://localhost:27017/mydatabase';
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;
